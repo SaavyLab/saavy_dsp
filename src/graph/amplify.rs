64b51d225f264661b1c7cc0d10e92866d62230b2
Saavy <jayson@saavylab.com>
2025-10-13 16:46:09 -0300

    updated readme and fixed Amplify.is_active()

--- src/graph/amplify.rs
+++ src/graph/amplify.rs
@@ -46,6 +46,6 @@ impl<N: GraphNode, M: GraphNode> GraphNode for Amplify<N, M> {
     }
 
     fn is_active(&self) -> bool {
-        self.modulator.is_active()
+        self.modulator.is_active() | self.signal.is_active()
     }
 }
