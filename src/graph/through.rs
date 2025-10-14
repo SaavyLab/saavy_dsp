e4bb003ce0be52889eb060c68e573557dd877e3e
Saavy <jayson@saavylab.com>
2025-10-14 20:09:35 -0300

    updated cpal demo to just play an arpeggio, fixed isactive check, formatted

--- src/graph/through.rs
+++ src/graph/through.rs
@@ -1,36 +1,33 @@
 use crate::graph::node::{GraphNode, RenderCtx};
 
 pub struct Through<S, F> {
-  source: S,
-  filter: F, 
+    source: S,
+    filter: F,
 }
 
 impl<S, F> Through<S, F> {
-  pub fn new(source: S, filter: F) -> Self {
-    Self {
-      source,
-      filter
+    pub fn new(source: S, filter: F) -> Self {
+        Self { source, filter }
     }
-  }
 }
 
 impl<S: GraphNode, F: GraphNode> GraphNode for Through<S, F> {
-  fn render_block(&mut self, out: &mut [f32], ctx: &RenderCtx) {
-      self.source.render_block(out, ctx);
-      self.filter.render_block(out, ctx);
-  }
+    fn render_block(&mut self, out: &mut [f32], ctx: &RenderCtx) {
+        self.source.render_block(out, ctx);
+        self.filter.render_block(out, ctx);
+    }
 
-  fn note_on(&mut self, ctx: &RenderCtx) {
-      self.source.note_on(ctx);
-      self.filter.note_on(ctx);
-  }
+    fn note_on(&mut self, ctx: &RenderCtx) {
+        self.source.note_on(ctx);
+        self.filter.note_on(ctx);
+    }
 
-  fn note_off(&mut self, ctx: &RenderCtx) {
-      self.source.note_off(ctx);
-      self.filter.note_off(ctx);
-  }
+    fn note_off(&mut self, ctx: &RenderCtx) {
+        self.source.note_off(ctx);
+        self.filter.note_off(ctx);
+    }
 
-  fn is_active(&self) -> bool {
-      self.source.is_active() | self.filter.is_active()
-  }
+    fn is_active(&self) -> bool {
+        self.source.is_active() || self.filter.is_active()
+    }
 }
